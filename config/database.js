07f716a82912f1a99a00fff86c5d4f3ccd5c24fd
Dixit3112 <dixit.dr123@gmail.com>
2024-08-20 15:38:34 +0530

    done for syncing database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize( 'database Name', 'username', 'database password', {options like host, dialect, logging, etc...})
 const sequelize = new Sequelize( 'postgres', 'postgres', 'Dixit@31091997', {
   host: "localhost",
   dialect: "postgres",
